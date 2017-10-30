b57894771d46f94b98541c90e82b8591ad8b7c52
Marcel Puyat <marcelpuyat@marcelpuyat-macbookpro.roam.corp.google.com>
2017-10-30 16:20:56 -0700

    Back to DCGAN architecture

--- model.py
+++ model.py
@@ -48,20 +48,10 @@ def DiscriminatorBeforeFullyConnectedLayer(X, instance_noise_std, reuse=False, n
 
 	D_bn1 = BatchNormalization(D_conv1, name='conv_bn1')
 	D_h1 = LeakyReLU(D_bn1)
-
-	extra_layer_conv = Conv2d(D_h1, output_dim=32, kernel=(3,3), strides=(1,1), name='conv_extra')
-	extra_layer_bn = BatchNormalization(extra_layer_conv, name="conv_extra_bn")
-	extra_layer_relu = LeakyReLU(extra_layer_bn)
-
-	D_conv2 = Conv2d(extra_layer_relu, output_dim=64, kernel=(3,3), name='conv2')
+	D_conv2 = Conv2d(D_h1, output_dim=64, kernel=(3,3), name='conv2')
 	D_bn2 = BatchNormalization(D_conv2, name='conv_bn2')
 	D_h2 = LeakyReLU(D_bn2)
-
-	extra_layer_conv_2 = Conv2d(D_h2, output_dim=64, kernel=(3,3), strides=(1,1), name='conv_extra_2')
-	extra_layer_bn_2 = BatchNormalization(extra_layer_conv_2, name="conv_extra_bn_2")
-	extra_layer_relu_2 = LeakyReLU(extra_layer_bn_2)
-
-	D_conv3 = Conv2d(extra_layer_relu_2, output_dim=128, kernel=(3,3), name='conv3')
+	D_conv3 = Conv2d(D_h2, output_dim=128, kernel=(3,3), name='conv3')
 	D_bn3 = BatchNormalization(D_conv3, name='conv_bn3')
 	D_h3 = LeakyReLU(D_bn3)
 	D_conv4 = Conv2d(D_h3, output_dim=256, kernel=(3,3), name='conv4')
@@ -94,31 +84,19 @@ def Generator(z, reuse=False, name='g'):
 		with tf.name_scope('deconv1_activation'):
 			variable_summaries(G_h2)
 
-		extra_layer_conv = Conv2d(G_h2, output_dim=1024, kernel=(3,3), strides=(1,1), name='conv_extra')
-		extra_layer_bn = BatchNormalization(extra_layer_conv, name="conv_extra_bn")
-		extra_layer_relu = LeakyReLU(extra_layer_bn)
-
-		G_conv3 = Deconv2d(extra_layer_relu, output_dim=256, batch_size=config.BATCH_SIZE, name='deconv2')
+		G_conv3 = Deconv2d(G_h2, output_dim=256, batch_size=config.BATCH_SIZE, name='deconv2')
 		G_bn3 = BatchNormalization(G_conv3, name='deconv2_bn')
 		G_h3 = tf.nn.relu(G_bn3)
 		with tf.name_scope('deconv2_activation'):
 			variable_summaries(G_h3)
 
-		extra_layer_conv_2 = Conv2d(G_h3, output_dim=512, kernel=(3,3), strides=(1,1), name='conv_extra_2')
-		extra_layer_bn_2 = BatchNormalization(extra_layer_conv_2, name="conv_extra_bn_2")
-		extra_layer_relu_2 = LeakyReLU(extra_layer_bn_2)
-
-		G_conv4 = Deconv2d(extra_layer_relu_2, output_dim=128, batch_size=config.BATCH_SIZE, name='deconv3')
+		G_conv4 = Deconv2d(G_h3, output_dim=128, batch_size=config.BATCH_SIZE, name='deconv3')
 		G_bn4 = BatchNormalization(G_conv4, name='deconv3_bn')
 		G_h4 = tf.nn.relu(G_bn4)
 		with tf.name_scope('deconv3_activation'):
 			variable_summaries(G_h4)
 
-		extra_layer_conv_3 = Conv2d(G_h4, output_dim=256, kernel=(3,3), strides=(1,1), name='conv_extra_3')
-		extra_layer_bn_3 = BatchNormalization(extra_layer_conv_3, name="conv_extra_bn_3")
-		extra_layer_relu_3 = LeakyReLU(extra_layer_bn_3)
-
-		G_conv5 = Deconv2d(extra_layer_relu_3, output_dim=4, batch_size=config.BATCH_SIZE, name='deconv5')
+		G_conv5 = Deconv2d(G_h4, output_dim=4, batch_size=config.BATCH_SIZE, name='deconv4')
 		G_r5 = tf.reshape(G_conv5, [config.BATCH_SIZE, 64*64*4])
 		tanh_layer = tf.nn.tanh(G_r5)
 		with tf.name_scope('tanh'):
@@ -133,7 +111,7 @@ def ModeEncoder(x, name='e'):
 	with tf.variable_scope(name):
 		D,_,_ = DiscriminatorBeforeFullyConnectedLayer(x, 0, False, name='Encoder')
 		D_h6 = Dense(D, output_dim=config.Z_DIM, name='dense')
-		predicted_z = tf.nn.sigmoid(D_h6, name='predictedZ')
+		predicted_z = tf.nn.tanh(D_h6, name='predictedZ')
 		with tf.name_scope('predictedZScope'):
 			variable_summaries(predicted_z)
 		return predicted_z
@@ -146,14 +124,14 @@ def GeneratorWithEmbeddings(z, embeddings, reuse, name='g'):
 	# 	Then deconv with stride 2, 5x5 filters into 4*32*32
 	#   tanh
 	with tf.variable_scope(name):
-		embeddings_with_noise = embeddings + tf.random_normal(shape=tf.shape(embeddings), mean=0, stddev=0.002, dtype=tf.float32)
-		embeddings = tf.nn.dropout(embeddings_with_noise, 0.2)
+		embeddings_with_noise = embeddings + tf.random_normal(shape=tf.shape(embeddings), mean=0, stddev=0.001, dtype=tf.float32)
+		embeddings = tf.nn.dropout(embeddings_with_noise, 0.5)
 		z_with_embeddings = tf.concat([z, embeddings], 1)
 		return Generator(z_with_embeddings, reuse, name)
 
 def DiscriminatorWithEmbeddings(X, embeddings, instance_noise_std, reuse=False, name='d'):
 	with tf.variable_scope(name, reuse=reuse):
-		embeddings_with_noise = embeddings + tf.random_normal(shape=tf.shape(embeddings), mean=0, stddev=0.002, dtype=tf.float32)
+		embeddings_with_noise = embeddings + tf.random_normal(shape=tf.shape(embeddings), mean=0, stddev=0.001, dtype=tf.float32)
 		D_r, D_h3_conv, minibatch_features = DiscriminatorBeforeFullyConnectedLayer(X, instance_noise_std, reuse, name)
 		D_h6_with_embeddings = tf.concat([D_r, embeddings_with_noise], 1)
 		D_h6 = Dense(D_h6_with_embeddings, output_dim=1, name='dense')
