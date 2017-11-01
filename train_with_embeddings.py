55c48fe4f9e67fbf00e6a5e8e201649a9b0e8310
Marcel Puyat <marcelpuyat@marcelpuyat-macbookpro.roam.corp.google.com>
2017-11-01 13:25:08 -0700

    First attempt at emojemoji_batch discrim

--- train_with_embeddings.py
+++ train_with_embeddings.py
@@ -7,6 +7,7 @@ import sys
 import warnings
 warnings.simplefilter('error', UserWarning)
 from random import shuffle
+from random import choice
 import json
 from PIL import Image
 from scipy.misc import imread, imresize
@@ -23,10 +24,15 @@ import config
 import utils
 
 image_metadata = []
-
+same_label_images = {}
 def get_image_metadata():
 	with open('sanitized_emoji_images_high_quality_medium.json') as data_file:    
 		data = json.load(data_file)
+		for d in data:
+			if d['title'] not in same_label_images:
+				same_label_images[d['title']] = [d['filename']]
+			else:
+				same_label_images[d['title']].append(d['filename'])
 		shuffle(data)
 		return data
 
@@ -43,7 +49,7 @@ def get_pixels_for_filename(filename):
 	return np.array(img)
 
 curr_image_idx = 0
-def get_next_image_batch(batch_size):
+def get_next_image_batch(batch_size, same_labels=False):
 	global curr_image_idx
 	global image_metadata
 	global word_vectors
@@ -51,50 +57,61 @@ def get_next_image_batch(batch_size):
 	pixels_batch = np.zeros(shape=(batch_size, config.IMAGE_SIZE))
 	embeddings_batch = np.zeros(shape=(batch_size, config.WORD_EMBEDDING_DIM))
 	labels = []
+
+	rand_label = choice(same_label_images.keys())
+	curr_same_label_index = 0
 	for i in range(batch_size):
 
-		# Keep looping til we get an image. Sometimes we'll find an image with an invalid size.
-		# TODO: Just sanitize the data
-		while True:
-
-			# Go back to the start, and then shuffle the images
-			if curr_image_idx >= len(image_metadata):
-				curr_image_idx = 0
-				shuffle(image_metadata)
-			# Note that we don't store all pixels in memory bec of memory constraints
-
-			try: # Another hack bec the metadata has stale files that have been deleted
-				pix = get_pixels_for_filename(image_metadata[curr_image_idx]['filename'])
-			except:
-				curr_image_idx += 1
-				continue
-			if pix.shape != (128, 128, 4):
-				print('Invalid pixels shape for file ' + image_metadata[curr_image_idx]['filename'] + ': ' + str(pix.shape) + ". Skipping.")
-				# Skip this image
-				curr_image_idx += 1
-				continue
-
-			label = image_metadata[curr_image_idx]['title']
-			if label not in word_vectors:
-				print("Didn't find " + label + " in word vectors. Skipping.")
-				curr_image_idx += 1
-				continue
+		if same_labels:
+			if curr_same_label_index >= len(same_label_images[rand_label]):
+				curr_same_label_index = 0
+			pix = get_pixels_for_filename(same_label_images[rand_label][curr_same_label_index])
+			labels.append(rand_label)
 			pixels_batch[i] = pix.reshape([config.IMAGE_SIZE])
-			embeddings_batch[i] = np.array(word_vectors[label])
-			labels.append(label)
-			curr_image_idx += 1
-			break
+			embeddings_batch[i] = np.array(word_vectors[rand_label])
+			curr_same_label_index += 1
+		else:
+			# Keep looping til we get an image. Sometimes we'll find an image with an invalid size.
+			# TODO: Just sanitize the data
+			while True:
+
+				# Go back to the start, and then shuffle the images
+				if curr_image_idx >= len(image_metadata):
+					curr_image_idx = 0
+					shuffle(image_metadata)
+				# Note that we don't store all pixels in memory bec of memory constraints
+
+				try: # Another hack bec the metadata has stale files that have been deleted
+					pix = get_pixels_for_filename(image_metadata[curr_image_idx]['filename'])
+				except:
+					curr_image_idx += 1
+					continue
+				if pix.shape != (128, 128, 4):
+					print('Invalid pixels shape for file ' + image_metadata[curr_image_idx]['filename'] + ': ' + str(pix.shape) + ". Skipping.")
+					# Skip this image
+					curr_image_idx += 1
+					continue
+
+				label = image_metadata[curr_image_idx]['title']
+				if label not in word_vectors:
+					print("Didn't find " + label + " in word vectors. Skipping.")
+					curr_image_idx += 1
+					continue
+				pixels_batch[i] = pix.reshape([config.IMAGE_SIZE])
+				embeddings_batch[i] = np.array(word_vectors[label])
+				labels.append(label)
+				curr_image_idx += 1
+				break
 	return pixels_batch, embeddings_batch, labels
 
 X = tf.placeholder(tf.float32, shape=[config.BATCH_SIZE, config.IMAGE_SIZE], name="real_images_input")
 z = tf.placeholder(tf.float32, shape=[config.BATCH_SIZE, 100], name="generator_latent_space_input")
 embeddings = tf.placeholder(tf.float32, shape=[config.BATCH_SIZE, config.WORD_EMBEDDING_DIM], name="embeddings_input")
-noisy_embeddings = tf.placeholder(tf.float32, shape=[config.BATCH_SIZE, config.WORD_EMBEDDING_DIM], name="noisy_embeddings_input")
 instance_noise_std = tf.placeholder(tf.float32, shape=(), name="instance_noise_std")
 
 G = GeneratorWithEmbeddings(z, embeddings, False, 'Generator')
-D_real_prob, D_real_logits, feature_matching_real, minibatch_similarity_real = DiscriminatorWithEmbeddings(X, noisy_embeddings, instance_noise_std, False, 'Discriminator')
-D_fake_prob, D_fake_logits, feature_matching_fake, minibatch_similarity_fake = DiscriminatorWithEmbeddings(G, noisy_embeddings, instance_noise_std, True, 'Discriminator')
+D_real_prob, D_real_logits, feature_matching_real, minibatch_similarity_real = DiscriminatorWithEmbeddings(X, embeddings, instance_noise_std, False, 'Discriminator')
+D_fake_prob, D_fake_logits, feature_matching_fake, minibatch_similarity_fake = DiscriminatorWithEmbeddings(G, embeddings, instance_noise_std, True, 'Discriminator')
 predicted_z = ModeEncoderWithEmbeddings(X, embeddings, 'ModeEncoder')
 image_from_predicted_z = GeneratorWithEmbeddings(predicted_z, embeddings, True, 'Generator')
 l2_distance_encoder = tf.sqrt(tf.reduce_sum(tf.square(tf.subtract(X, image_from_predicted_z))))
@@ -142,7 +159,8 @@ feature_matching_lambda = 0.07
 l2_distance_encoder *= encoder_lambda_1
 mode_regularizer_loss *= encoder_lambda_2
 feature_matching_loss *= feature_matching_lambda
-G_loss = D_fake_wrong + l2_distance_encoder + mode_regularizer_loss + feature_matching_loss
+minibatch_discrim_loss = tf.nn.l2_loss(minibatch_similarity_real - minibatch_similarity_fake)
+G_loss = D_fake_wrong + l2_distance_encoder + mode_regularizer_loss + feature_matching_loss + minibatch_discrim_loss
 E_loss = l2_distance_encoder + mode_regularizer_loss
 
 tf.summary.scalar("D_real_loss", D_real)
@@ -157,6 +175,7 @@ tf.summary.scalar("l2_distance_encoder", l2_distance_encoder)
 tf.summary.scalar("instance_noise_std", instance_noise_std)
 tf.summary.scalar("minibatch_similarity_real", tf.reduce_mean(minibatch_similarity_real))
 tf.summary.scalar("minibatch_similarity_fake", tf.reduce_mean(minibatch_similarity_fake))
+tf.summary.scalar("minibatch_similarity_loss", minibatch_discrim_loss)
 tf.summary.scalar("d_real_prob", tf.reduce_mean(D_real_prob))
 tf.summary.scalar("d_fake_prob", tf.reduce_mean(D_fake_prob))
 
@@ -213,23 +232,20 @@ with tf.Session() as sess:
 			instance_noise_std_value = get_instance_noise_std(curr_step)
 
 			x, label_embeddings, labels = get_next_image_batch(config.BATCH_SIZE)
-			noisy_label_embeddings = np.zeros((config.BATCH_SIZE, config.WORD_EMBEDDING_DIM))
-			# Give non-sensical label to some objects in batch
-			for i in range(len(labels)):
-				if np.random.rand() < 0.1:
-					noisy_label_embeddings[i] = np.random.normal(0, 0.16, 300)
-				else:
-					noisy_label_embeddings[i] = label_embeddings[i]
+			x_g, label_embeddings_g, labels_g = get_next_image_batch(config.BATCH_SIZE, same_labels=True)
 			x = utils.normalize_image_batch(x)
+			x_g = utils.normalize_image_batch(x_g)
 
 			rand = latent_space_sampler.rvs((config.BATCH_SIZE, config.Z_DIM))
-			feed_dict = {X: x, z: rand, instance_noise_std: instance_noise_std_value, embeddings: label_embeddings, noisy_embeddings: noisy_label_embeddings}
+			feed_dict = {X: x, z: rand, instance_noise_std: instance_noise_std_value, embeddings: label_embeddings}
+			feed_dict_g = {X: x_g, z: rand, instance_noise_std: instance_noise_std_value, embeddings: label_embeddings_g}
 			_, D_loss_curr = sess.run([disc_optimizer, D_loss], feed_dict)
 
 			if curr_step > 0 and curr_step % config.STEPS_PER_SUMMARY == 0:
 				summary, _, _, G_loss_curr = sess.run([merged, generator_optimizer, encoder_optimizer, G_loss], feed_dict)
 				train_writer.add_summary(summary, curr_step)
 			else:
+				sess.run([generator_optimizer, encoder_optimizer], feed_dict_g)
 				_, _, G_loss_curr = sess.run([generator_optimizer, encoder_optimizer, G_loss], feed_dict)
 
 			sys.stdout.write("\rstep %d: %f, %f" % (curr_step, D_loss_curr, G_loss_curr))
@@ -240,6 +256,10 @@ with tf.Session() as sess:
 				# Note that these samples have "pixels" in the range (-1,1)
 				generated_samples = sess.run(G, {z: rand, embeddings: label_embeddings})
 				utils.save_samples_labeled(generated_samples, labels, curr_step / config.STEPS_PER_IMAGE_SAMPLE)
+
+				# This is for same labels
+				generated_samples = sess.run(G, {z: rand, embeddings: label_embeddings_g})
+				utils.save_samples_labeled(generated_samples, labels, curr_step / config.STEPS_PER_IMAGE_SAMPLE + 1000)
 
 			if curr_step > 0 and curr_step % config.STEPS_PER_SAVE == 0:
 				utils.save(config.CHECKPOINT_DIR, curr_step, sess, saver)
